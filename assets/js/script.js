d16cf29e1ec92273f2625ddff36dcef5ec9a8bd7
kylewwelsh <kyle@kdmdental.com>
2021-05-12 20:02:28 -0600

    javascript started, console logging keys, broke grid (fix later)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,40 @@
 'use strict'
 const calculator = document.querySelector('.calculator');
-const calculatorButtons = document.querySelector('button');
+const calculatorButtons = calculator.querySelector('.calculatorButtons');
 
 calculatorButtons.addEventListener (
   'click', e => {
-    if (e.target.matches)
+    if (e.target.matches('button')) {
+    
+    const key = e.target
+    const action = key.dataset.action
+
+    if (!action) {
+      console.log('number key!')
+    }
+
+    if (
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'
+    ) {
+      console.log('operator key!')
+    }
+
+    if (action === 'decimal') {
+      console.log('decimal key!')
+    }
+
+    if (action === 'clear') {
+      console.log('clear key!')
+    }
+
+    if (action === 'calculate') {
+      console.log('equal key!')
+    }
+
+    
+
   }
-)
+});
