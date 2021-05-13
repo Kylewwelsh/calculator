81f8dac370030fa61c659aeb5a105fe76a56e8da
kylewwelsh <kyle@kdmdental.com>
2021-05-12 19:32:27 -0600

    class added to calculator buttons

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+'use strict'
+const calculator = document.querySelector('.calculator');
+const calculatorButtons = document.querySelector('button');
+
+calculatorButtons.addEventListener (
+  'click', e => {
+    if (e.target.matches)
+  }
+)
